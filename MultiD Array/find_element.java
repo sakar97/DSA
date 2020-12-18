d3fd1220b8f51e47f3f1fdfa89079ad7cae38086
sakar97 <sakar.mehra@gslab.com>
2020-12-18 23:42:28 +0530

    daily updates

--- MultiD Array/find_element.java
+++ MultiD Array/find_element.java
@@ -1,4 +1,4 @@
-// IN: [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target = 13
+// IN: [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target = 23
 // OUT: false
 public class find_element {
     public static void main(String args[]) {
