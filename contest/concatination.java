bf2b52c528291baceea498a2ad45150b775b0f25
sakar97 <sakar.mehra@gslab.com>
2021-01-01 21:39:47 +0530

    updates

---
+++ contest/concatination.java
@@ -0,0 +1,26 @@
+class concatination {
+    public boolean canFormArray(int[] arr, int[][] pieces) {
+        Map<Integer, int[]> map=new HashMap<>();
+        for(int[] piece: pieces){
+            map.put(piece[0],piece);
+        }
+        int start=0;
+        while(start<arr.length){
+            if(map.containsKey(arr[start])){
+                int[] piece=map.get(arr[start]);
+                for(int i=0;i<piece.length;i++){
+                    if(arr[start]!=piece[i]){
+                        return false;
+                    }
+                    else{
+                        start++;
+                    }
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
