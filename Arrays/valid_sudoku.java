66b7901656a24747c9421db4f68c7d4d7f455dcc
sakar97 <sakar.mehra@gslab.com>
2021-01-12 23:32:51 +0530

    updates

--- Arrays/valid_sudoku.java
+++ Arrays/valid_sudoku.java
@@ -1,13 +1,14 @@
+import java.util.*;
+
 class valid_sudoku {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> hs=new HashSet<>();
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                char current_val=board[i][j];
-                if(current_val!='.'){
-                    if(!hs.add(current_val+"found in row " +i)||
-                       !hs.add(current_val+"found in column "+j)||
-                       !hs.add(current_val+"found in sub box "+i/3+"-"+j/3))
+        HashSet<String> hs = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char current_val = board[i][j];
+                if (current_val != '.') {
+                    if (!hs.add(current_val + "found in row " + i) || !hs.add(current_val + "found in column " + j)
+                            || !hs.add(current_val + "found in sub box " + i / 3 + "-" + j / 3))
                         return false;
                 }
             }
