274323328aacb7a9d2600ed810b55505e904094a
sakar97 <sakar.mehra@gslab.com>
2021-01-24 00:04:00 +0530

    updates

---
+++ Stack/NLE.java
@@ -0,0 +1,34 @@
+
+//Next Largest Element
+//O(N)== TC
+//IN: [1,3,2,4]
+//OUT:[3,4,4,-1]
+import java.util.*;
+
+class next_largest {
+    public static void main(String args[]) {
+        List<Integer> as = new ArrayList<>();
+        Stack<Integer> s = new Stack<>();
+        int[] arr = { 1, 3, 2, 4 };
+        int n = arr.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.size() == 0) {
+                as.add(-1);
+            } else if (s.size() > 0 && s.peek() > arr[i]) {
+                as.add(s.peek());
+            } else if (s.size() > 0 && s.peek() <= arr[i]) {
+                while (s.size() > 0 && s.peek() <= arr[i]) {
+                    s.pop();
+                }
+                if (s.size() == 0) {
+                    as.add(-1);
+                } else {
+                    as.add(s.peek());
+                }
+            }
+            s.push(arr[i]);
+        }
+        Collections.reverse(as);
+        System.out.println(as);
+    }
+}
