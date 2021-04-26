0ee8e4c0337313b6d852f13e42d2359b25dad35c
sakar97 <sakar.mehra@gslab.com>
2021-04-27 02:14:30 +0530

    update

--- DP/matrix_chain_multiplication.java
+++ DP/matrix_chain_multiplication.java
@@ -1,18 +1,19 @@
-public class{
+public class matrix_chain_multiplication {
     public static void main(String[] args) {
-        int[] n= {40, 20, 30, 10, 30};
-        int i=1;
-        int j=n.length-1;
-        System.out.print(solve(n,i,j));
+        int[] n = { 40, 20, 30, 10, 30 };
+        int i = 1;
+        int j = n.length - 1;
+        System.out.print(solve(n, i, j));
     }
-    public int solve(int[] arr,int i,int j){
-        if(i>=j)
+
+    public static int solve(int[] arr, int i, int j) {
+        if (i >= j)
             return 0;
-        int min=Integer.MIN_VALUE;
-        for(int k=i;k<j-1;k++){
-            int tempValue=solve(arr,i,k)+solve(arr,k+1,j)+(arr[i-1]*arr[k]*arr[j]);
-            if(temp<min){
-                min=temp;
+        int min = Integer.MAX_VALUE;
+        for (int k = i; k < j; k++) {
+            int tempValue = solve(arr, i, k) + solve(arr, k + 1, j) + (arr[i - 1] * arr[k] * arr[j]);
+            if (tempValue < min) {
+                min = tempValue;
             }
         }
         return min;
