a10ee46cbad52dbdf37417d4984b05da3a8342b0
sakar97 <sakar.mehra@gslab.com>
2021-04-28 01:03:29 +0530

    update

---
+++ DP/matrix_chain_multiplicationII.java
@@ -0,0 +1,24 @@
+public class matrix_chain_multiplicationII {
+
+    static int[][] dp = new int[1001][1001];
+
+    public static void main(String[] args) {
+        int[] n = { 40, 20, 30, 10, 30 };
+        int i = 1;
+        int j = n.length - 1;
+        System.out.println(solve(n, i, j));
+    }
+
+    public static int solve(int[] arr, int i, int j) {
+        if (i >= j)
+            return 0;
+        int min = Integer.MAX_VALUE;
+        for (int k = i; k < j; k++) {
+            int tempValue = solve(arr, i, k) + solve(arr, k + 1, j) + (arr[i - 1] * arr[k] * arr[j]);
+            if (tempValue < min) {
+                min = tempValue;
+            }
+        }
+        return dp[i][j] = min;
+    }
+}
